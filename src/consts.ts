311290bb3b30489c0a904a911570266655230f74
Gareth Lewis <gareth-lewis@users.noreply.github.com>
2023-02-28 12:36:52 +0000

    Site structure and home page

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'IDS Data Explorer';
+export const SITE_DESCRIPTION = 'A better way to connect with government data. The Integrated Data Service (IDS) is bringing together data from across the UK government and Devolved Administrations, providing new ways to give you the data you need.';
+export const SITE_PHASE = 'Prototype';
